f30d0ac58d54c741a602da55429927b879961005
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2021-06-23 16:27:56 -0500

    add a different example

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,3 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default () => <button>Test</button>
+const useNotifications = () => {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      const response = await fetch('/api/notifications');
+      const data = await response.json();
+
+      setNotifications(data.notifications);
+    };
+
+    const id = setInterval(() => fetchNotifications(), 200);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return notifications;
+}
+
+export const Button = () => {
+  const notifications = useNotifications();
+
+  return <button>Notifications: {notifications.length}</button>;
+};
