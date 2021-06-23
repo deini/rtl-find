5b0ee67ce440f325283d677a40eda4ff20d38b8f
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2021-06-23 15:59:51 -0500

    add a different example

--- src/spec.js
+++ src/spec.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 
-import Button from './Button';
+import { Button } from './Button';
+
+const server = setupServer(
+  rest.get('/api/notifications', (_req, res, ctx) =>
+    res(ctx.json({ notifications: ['Foo'] }))
+  )
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 afterEach(() => {
-  jest.runOnlyPendingTimers()
-  jest.useRealTimers()
-})
+  jest.runOnlyPendingTimers();
+  jest.useRealTimers();
+});
 
-test('Renders', async () => {
-  // Fails with both legacy / modern
+test('renders number of notifications', async () => {
   jest.useFakeTimers();
 
-  render(<Button />)
+  // Override the endpoint only for one call
+  server.use(
+    rest.get('/api/notifications', (_req, res, ctx) =>
+      res.once(ctx.json({ notifications: ['Foo', 'Bar'] }))
+    )
+  );
 
-  // Looking for a role that doesn't exist with a long timeout
-  // This should wait for 10 seconds but fails instantly with fake timers
-  const button = await screen.findByRole('dialog', {}, { timeout: 10000 });
+  render(<Button />);
 
-  expect(button).toBeInTheDocument();
+  // Initializes with 0 from useState([])
+  await screen.findByRole('button', { name: /Notifications: 0/i });
+
+  // Override endpoint (once) returns two notifications
+  act(() => jest.advanceTimersByTime(200));
+  await screen.findByRole('button', { name: /Notifications: 2/i });
+
+  // Default endpoint return one notification
+  act(() => jest.advanceTimersByTime(200));
+  await screen.findByRole('button', { name: /Notifications: 1/i });
 });
+
+// Note: This will still give us act warnings because we have a fetch inside
+// the interval, we could probably do an await act(() => await screen.find... )
